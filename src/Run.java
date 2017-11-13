4f6c3b122e291c7726215ac4e85260ed89653865
elisabethgombkoto <ego8769@students.fhv.at>
2017-11-13 12:09:17 +0100

    Initial commit

---
+++ src/Run.java
@@ -0,0 +1,10 @@
+import sun.beanbox.BeanBoxFrame;
+
+/**
+ * Created by Elisabeth on 13.11.2017.
+ */
+public class Run {
+  public static void main(String[] args) {
+    BeanBoxFrame.main(new String[]{});
+  }
+}
---
+++ src/filters/Test.java
@@ -0,0 +1,7 @@
+package filters;
+
+/**
+ * Created by Elisabeth on 13.11.2017.
+ */
+public class Test {
+}
